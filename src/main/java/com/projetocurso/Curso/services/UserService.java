1571f6fa0a2a616cf53608c5a468fefdf465dfb3
“fabiomendesdevl” <fabiomendesdev@outlook.com>
2025-04-26 15:40:02 -0300

    User update

--- src/main/java/com/projetocurso/Curso/services/UserService.java
+++ src/main/java/com/projetocurso/Curso/services/UserService.java
@@ -5,6 +5,7 @@ import com.projetocurso.Curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +30,17 @@ public class UserService {
 
     public void delete(Long id){
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
