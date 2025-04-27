83d68bd608ad076599d0feb21d99e1d282a195ae
“fabiomendesdevl” <fabiomendesdev@outlook.com>
2025-04-27 15:48:06 -0300

    exception update

--- src/main/java/com/projetocurso/Curso/services/UserService.java
+++ src/main/java/com/projetocurso/Curso/services/UserService.java
@@ -4,6 +4,7 @@ import com.projetocurso.Curso.entities.User;
 import com.projetocurso.Curso.repositories.UserRepository;
 import com.projetocurso.Curso.services.exceptions.DataBaseException;
 import com.projetocurso.Curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,10 +47,14 @@ public class UserService {
         }
     }
 
-    public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
